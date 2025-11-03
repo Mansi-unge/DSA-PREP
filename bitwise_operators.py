cb2e4f2725d973d81f13f1bb14a7789ce6fb0a4e
Mansi Unge <mansiunge@gmail.com>
2025-11-03 16:54:20 +0530

    bitwise operators done

---
+++ bitwise_operators.py
@@ -0,0 +1,19 @@
+# Bitwise Operators Program
+
+# Input numbers
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+
+print("\n=== BITWISE OPERATIONS ===")
+print("a :",a)
+print("b :",b)
+
+# AND Operator
+print("AND Operator :", a & b)
+
+print("OR Operator :", a | b)
+print("NOT Operator ~a:", ~a)
+print("NOT Operator ~b:", ~b)
+print("XOR Operator :", a ^ b)
+print("left shift Operator :", a << b)
+print("right shift Operator :", a >> b)
