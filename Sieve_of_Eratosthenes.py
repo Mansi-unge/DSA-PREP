d0fa4d21216fbb7f748032fe2672e05ae48a50a6
Mansi Unge <mansiunge@gmail.com>
2025-11-04 11:55:21 +0530

    sieve of eratosthenes done

---
+++ Sieve_of_Eratosthenes.py
@@ -0,0 +1,17 @@
+def sieve_of_eratosthenes(n):
+  primes = [True]*(n+1)
+  primes[0] = primes[1] = False
+
+  for i in range (2 , int(n**0.5)+1):
+    if primes[i]:
+      for j in range(i*i, n+1, i):
+        primes[j]=False
+
+  for i in range(2,n+1):
+    if primes[i]:
+      print(i,end=" ")
+
+n = int(input("enter any number :"))
+print("number you entred is :",n)
+
+sieve_of_eratosthenes(n)
